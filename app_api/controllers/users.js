883821105b3ed302c9f28a2bb05de1a17653f427
Oona <oona.salmipuro@metropolia.fi>
2016-09-23 16:23:25 +0300

    fixed REST problem

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -15,7 +15,29 @@ module.exports.readTeachers = function (req, res) {
 
 module.exports.readOneTeacher = function (req, res) {
 	//change this to actual data
-	sendJsonResponse(res, 200, {"status" : "success"});
+	//sendJsonResponse(res, 200, {"status" : "success"});
+
+	if(req.params && req.params.teacherid) {
+		t.findById(req.params.teacherid).exec(function(err, teacher) {
+	        //if teacher is not returned
+	        if(!teacher) {
+	        	sendJsonResponse(res, 404, {
+	        		"message": "teacherid not found"
+	        	});
+	        	return;
+	        //if error message is returned
+	    	} else if(err) {
+	    		sendJsonResponse(res, 404, err);
+	    		return;
+	   		}
+	    	sendJsonResponse(res, 200, teacher);
+	    });
+	} else {
+		sendJsonResponse(res, 404, {
+			"message": "No teacherid in request"
+		});
+	}
+
 };
 
 module.exports.addTeacher = function (req, res) {
