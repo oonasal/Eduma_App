81982786c0cdef87fa08423a3957223716d21c4e
Vi Elite <nmt1994@gmail.com>
2016-09-24 21:41:00 +0300

    Added login form

--- public/controllers/main.controller.js
+++ public/controllers/main.controller.js
@@ -1,20 +1,27 @@
-(function() {
-'use strict';
+(function(){
     var app = angular.module('myApp',[]);
-    app.controller('MyAppController', MyAppController);
 
-    MyAppController.$inject = ['$http'];
-    function MyAppController($http) {
+    
+    app.controller('MyAppController',['$scope','$http','AuthService',function ($scope,$http,authService) {
+        var vm = $scope;
+
+        vm.doSubmit = doSubmit;
+
+        function doSubmit() {
+            var email = vm.email;
+            var password = vm.password;
+
+            authService.login(email,password)  // promise me you will love me lololol - I mean use promise here
+            // console.log(email,password);
+        }        
+    }]);
+
+
+    app.controller('LoginController',['$scope','$http','AuthService',function($scope,$http,authService) {
         var vm = this;
+        console.log($http);
+        // AuthService.callFunc();
+    }]);
 
-        activate();
+})()
 
-        ////////////////
-
-        function activate() {
-            
-        }
-    }
-
-
-})();
