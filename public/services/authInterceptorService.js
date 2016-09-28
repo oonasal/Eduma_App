5aeedd0f088a23a408815c169c5f37863a97f28a
Tri Nguyen <nmt1994@gmail.com>
2016-09-28 23:05:11 +0300

    Teacher login testable

---
+++ public/services/authInterceptorService.js
@@ -0,0 +1,48 @@
+﻿angular.module('myApp').factory('authInterceptorService', ['$q', '$injector','$location', 'localStorageService', function ($q, $injector,$location, localStorageService) {
+
+    var authInterceptorServiceFactory = {};
+    var $http;
+
+    var _request = function (config) {
+
+        config.headers = config.headers || {};
+       
+        var authData = localStorageService.get('authorizationData');
+        if (authData) {
+            config.headers.Authorization = 'Bearer ' + authData.token;
+        }
+
+        return config;
+    }
+
+    var _responseError = function (rejection) {
+        var deferred = $q.defer();
+        if (rejection.status === 401) {
+            var authService = $injector.get('authService');
+            authService.refreshToken().then(function (response) {
+                _retryHttpRequest(rejection.config, deferred);
+            }, function () {
+                authService.logOut();
+                $location.path('/');
+                deferred.reject(rejection);
+            });
+        } else {
+            deferred.reject(rejection);
+        }
+        return deferred.promise;
+    }
+
+    var _retryHttpRequest = function (config, deferred) {
+        $http = $http || $injector.get('$http');
+        $http(config).then(function (response) {
+            deferred.resolve(response);
+        }, function (response) {
+            deferred.reject(response);
+        });
+    }
+
+    authInterceptorServiceFactory.request = _request;
+    authInterceptorServiceFactory.responseError = _responseError;
+
+    return authInterceptorServiceFactory;
+}]);
